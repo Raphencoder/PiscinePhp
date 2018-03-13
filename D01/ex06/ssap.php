f48a2cc9f508bbb672e1b75754776e411d5727b4
Raphael KRIEF <rkrief@e3r2p20.42.fr>
2018-03-13 18:17:59 +0100

    add ex06

---
+++ D01/ex06/ssap.php
@@ -0,0 +1,23 @@
+#!/usr/bin/php
+<?php
+$i = 1;
+$j = 0;
+$l = 0;
+if ($argc < 2)
+	return (0);
+while ($argv[$i])
+{
+	$tab = array_filter(explode(" ", $argv[$i]));
+	$i++;
+	$j = 0;
+	while($tab[$j])
+		$final[$l++]=$tab[$j++];
+}
+sort($final);
+$i = 0;
+while($final[$i])
+{
+	print_r($final[$i++]);
+	echo "\n";
+}
+?>
