c38b3aad10fc22cf1a96894e6e63958a112e3151
Raphael KRIEF <rkrief@e2r3p3.42.fr>
2018-03-28 11:46:34 +0200

    add the 0 function

--- D01/ex03/ft_split.php
+++ D01/ex03/ft_split.php
@@ -1,8 +1,13 @@
-#!/usr/bin/php
 <?php
+
+function ft_zero($var){
+	return ($var !== FALSE && $var !== NULL && $var !== '');
+}
+
 function ft_split($str)
 {
-	$str = (array_filter(explode(" ", $str)));
+	$str = array_filter(explode(" ", $str), 'ft_zero');
 	sort($str);
+	return ($str);
 }
 ?>
