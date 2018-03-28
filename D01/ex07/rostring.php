ad866294b1910389ae9b9eef8aa587520b8c5b36
Raphael KRIEF <rkrief@e2r3p2.42.fr>
2018-03-28 14:09:14 +0200

    add fot the 0

--- D01/ex07/rostring.php
+++ D01/ex07/rostring.php
@@ -1,10 +1,13 @@
 #!/usr/bin/php
 <?php
+function ft_zero($var){
+	return ($var !== FALSE && $var !== NULL && $var !== '');
+}
 if ($argc < 2)
 	return (0);
 unset($argv[0]);
 $new = trim($argv[1]);
-$tab = array_filter(explode(" ", $new));
+$tab = array_filter(explode(" ", $new), 'ft_zero');
 $first = " ".$tab[0];
 unset ($tab[0]);
 $final = implode(" ", $tab);
